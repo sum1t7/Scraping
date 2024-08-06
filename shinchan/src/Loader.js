6263eefe921df872d764ee68aa9aa22482ac19ae
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2024-08-06 04:22:19 +0000

    toggles-added

--- shinchan/src/Loader.js
+++ shinchan/src/Loader.js
@@ -10,7 +10,7 @@ const VideoPlayer = () => {
     const [videoUrl, setVideoUrl] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
+    const [NotAvailable, setNotAvailable] = useState(false);
 
 
 const fetchVideoUrl = () => {
@@ -33,8 +33,48 @@ const fetchVideoUrl = () => {
         setLoading(false);
       });
   };
+  
+  let current = episode;
+  let currentSeason = season;
 
- 
+  const previous = () =>{
+    if(currentSeason == 1 && current < 53){
+      setNotAvailable(true);
+      current = 2;
+      currentSeason = 2;
+      setEpisode(current);
+      setSeason(currentSeason);
+ }
+   if (current <= 1) {
+    setSeason(--currentSeason);
+    current = 53;
+    setEpisode(current);
+  }
+  setNotAvailable(false);
+    setEpisode(--current);
+    fetchVideoUrl()
+}
+// seasons upto 15 , episodes upto 52 ,Not working: season 9 , season 10 ,season 11
+const next = () =>{
+  
+  if (current >= 52) {
+    setSeason(++currentSeason);
+    current = 0;
+    setEpisode(current);
+  }
+
+  if(currentSeason > 15){
+    setNotAvailable(true);
+    current = 0;
+    currentSeason = 2;
+    setEpisode(current);
+    setSeason(currentSeason);
+  }
+
+  setNotAvailable(false);
+  setEpisode(++current);
+  fetchVideoUrl();
+}
 
   return (
     videoUrl == '' ? <div className='InputFields'>
@@ -62,17 +102,23 @@ const fetchVideoUrl = () => {
     </div> 
     
     :
+    <div>
     <div className='videoplayer InputFields'>
-      
       {loading && <p>Loading...</p>}
       <h1>Season:{season} Episode:{episode}</h1>
       <iframe src={videoUrl}   width={800} height={500} frameborder="0" allowfullscreen allow='fullscreen'>
       </iframe>
-
+    </div>
+    
+    <div className='change'>
+      <button className='change_buttons' onClick={previous} >Previous</button>
+      <button className='change_buttons' onClick={next} >Next</button>
+    </div>
+    {NotAvailable && <p>Not Available</p>}
     </div>
   );
    
 }
-    
+
 export default VideoPlayer;
  

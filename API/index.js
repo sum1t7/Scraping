291531e1fdcff1b8a474a0ccab576e5d13076bb7
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2024-10-26 11:51:02 +0530

    fix

--- API/index.js
+++ API/index.js
@@ -12,7 +12,6 @@ app.use(cors({
 app.get('/video', (req, res) => {
     const { season, episode } = req.query;
     const videoUrl = `https://beta.awstream.net/watch?v=shinchan-8211-season-${season}-8211-episode-${episode}&lang=hin`;
-    videoUrl = season === '01' ? `https://beta.awstream.net/watch?v=shinchan-8211-episode-${episode}&lang=hin`: videoUrl;
     res.json({ videoUrl , season, episode});
    
 });

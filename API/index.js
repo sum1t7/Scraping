734534ef40ae3244a999297a816083d08c21f10e
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-05-11 16:46:44 +0530

    source_change

--- API/index.js
+++ API/index.js
@@ -21,7 +21,7 @@ app.get("/api/:name/:season/:episode", async (req, res) => {
   const {name, season, episode } = req.params;
 
   try {
-    const url = `https://anime-world.co/episode/${name}-${season}x${episode}/`;
+    const url = `https://anime-world.cc/episode/${name}-${season}x${episode}/`;
 
     const { data } = await axios.get(url);
 
@@ -41,6 +41,38 @@ app.get("/api/:name/:season/:episode", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+
+app.get("/images/:name/:seasonId/:episode", async (req, res) => {
+  const { name, seasonId, episode } = req.params;
+
+  try {
+    const url = `https://anime-world.cc/series/${name}`;
+
+    const { data } = await axios.get(url);
+
+    if (data.includes("404 Not Found")) {
+      return res.status(404).json({ error: "Page not found" });
+    }
+
+    const $ = load(data);
+    const imgage = [];
+$('li article div.post-thumbnail img').each((index, element) => {
+  const src = $(element).attr('src');
+  if (src) {
+    imgage.push(src);
+  }
+});
+
+    return image
+      ? res.json({ image })
+      : res.status(404).json({ error: "Image not found" });
+  } catch (error) {
+    console.error("Error scrapping", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
